e687b9a81031c41a514774fbc9a7cd5d2a1423b1
Yibo Kong <k825379114@gmail.com>
2024-02-25 20:03:47 -0600

    changed length() to eachindex()

--- src/PhyloClustering.jl
+++ src/PhyloClustering.jl
@@ -1,22 +1,22 @@
 __precompile__()
 
 module PhyloClustering
-    include("bipartition.jl")
-    include("helper.jl")
-    include("models.jl")
+include("bipartition.jl")
+include("helper.jl")
+include("models.jl")
 
-    export 
-        kmeans_label, 
-        gmm_label, 
-        hc_label, 
-        dbscan_label,
-        standardize_tree,
-        plot_clusters,
-        distance,
-        num_bipartitions,
-        show_bipartitions,
-        show_bipartition,
-        get_bipartition,
-        split_weight,
-        num_to_name
+export
+    kmeans_label,
+    gmm_label,
+    hc_label,
+    dbscan_label,
+    standardize_tree,
+    plot_clusters,
+    distance,
+    num_bipartitions,
+    show_bipartitions,
+    show_bipartition,
+    get_bipartition,
+    split_weight,
+    num_to_name
 end
